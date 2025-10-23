da68f3983903d3683c304f970e0d2068c16cc840
Pineapples0001 <md.tanzim.hossain.2001@gmail.com>
2025-10-24 05:10:59 +0600

    Define OrderItemDisplay custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,32 @@ void main() {
   runApp(const App());
 }
 
-  class App extends StatelessWidget {
-    const App({super.key});
-@override
-Widget build(BuildContext context) {
-  return MaterialApp(
-    title: 'Sandwich Shop App',
-    home: Scaffold(
-      appBar: AppBar(title: const Text('Sandwich Counter')),
-      body: const Center(
-        child: Text('Welcome to the Sandwich Shop!'),
+class App extends StatelessWidget {
+  const App({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sandwich Shop App',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Sandwich Counter')),
+        body: const Center(
+          child: Text('Welcome to the Sandwich Shop!'),
+        ),
       ),
-    ),
-  );
-}
+    );
+  }
+  }
+
+class OrderItemDisplay extends StatelessWidget {
+  final String itemType;
+  final int quantity;
+
+  const OrderItemDisplay(this.quantity, this.itemType, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('This is a placeholder for OrderItemDisplay');
+  }
 }
 
 class MyApp extends StatelessWidget {
