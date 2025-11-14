9f639187124cda929be67e071d6e13398f6e894d
Pineapples0001 <md.tanzim.hossain.2001@gmail.com>
2025-11-14 07:14:12 +0600

    Implement counter functionality with setState

--- lib/main.dart
+++ lib/main.dart
@@ -15,39 +15,6 @@ class App extends StatelessWidget {
     );
   }
 }
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Sandwich Shop App',
-      home: Scaffold(
-      appBar: AppBar(title: const Text('Sandwich Counter')),
-      // The bit that you need to update starts from here
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const OrderItemDisplay(5, 'Footlong'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () => print('Add button pressed!'),
-                  child: const Text('Add'),
-                ),
-                ElevatedButton(
-                  onPressed: () => print('Remove button pressed!'),
-                  child: const Text('Remove'),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-      // The bit that you need to update ends here
-    ),
-    );
-  }
-
 
 class OrderScreen extends StatefulWidget {
   final int maxQuantity;
@@ -63,17 +30,17 @@ class OrderScreen extends StatefulWidget {
 class _OrderScreenState extends State<OrderScreen> {
   int _quantity = 0;
 
-void _increaseQuantity() {
-  if (_quantity < widget.maxQuantity) {
-    setState(() => _quantity++);
+  void _increaseQuantity() {
+    if (_quantity < widget.maxQuantity) {
+      setState(() => _quantity++);
+    }
   }
-}
 
-void _decreaseQuantity() {
-  if (_quantity > 0) {
-    setState(() => _quantity--);
+  void _decreaseQuantity() {
+    if (_quantity > 0) {
+      setState(() => _quantity--);
+    }
   }
-}
 
 @override
 Widget build(BuildContext context) {
@@ -93,11 +60,11 @@ Widget build(BuildContext context) {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               ElevatedButton(
-                onPressed: () => print('Add button pressed!'),
+                onPressed: _increaseQuantity,
                 child: const Text('Add'),
               ),
               ElevatedButton(
-                onPressed: () => print('Remove button pressed!'),
+                onPressed: _decreaseQuantity,
                 child: const Text('Remove'),
               ),
             ],
@@ -117,6 +84,7 @@ class OrderItemDisplay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('$quantity $itemType sandwich(es): ${'🥪' * quantity}');
+    final sandwiches = List.filled(quantity, '🥪').join();
+    return Text('$quantity $itemType sandwich(es): $sandwiches');
   }
 }
